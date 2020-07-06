5e9bd5fc335bf60e0221ef4d06407337108a8f0b
dvru <damvaru4@gmail.com>
2020-07-06 13:42:31 -0500

    added google test

---
+++ test/script.js
@@ -0,0 +1,17 @@
+const googleDatabase = [
+    'cars.com',
+    'recipes.com',
+    'flowers.com',
+    'dogs.com',
+    'carpics.com',
+    'myfavcars.com'
+];
+
+const googleSearch = (searchInput) => {
+    const matches = googleDatabase.filter(website => {
+        return website.includes(searchInput);
+    })
+    return matches.length > 3 ? matches.slice(0, 3) : matches
+};
+
+console.log(googleSearch('flow'));
