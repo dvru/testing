137a0f6e90af22932703a8ec62ef5e26da9f9037
dvru <damvaru4@gmail.com>
2020-07-06 14:08:30 -0500

    grouped tests

--- test/script.test.js
+++ test/script.test.js
@@ -7,11 +7,25 @@ dbMock = [
     'housepics.com'
 ];
 
-it('this is a test', () => {
-    expect('hello').toBe('hello')
+describe('googleSearch', () => {
+    it('this is a test', () => {
+        expect('hello').toBe('hello')
+    });
+    
+    it('it is searching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([])
+        expect(googleSearch('house', dbMock)).toEqual(['house.com', 'housepics.com'])
+    });
+    
+    it('work undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    });
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    });
 });
 
-it('it is searching google', () => {
-    expect(googleSearch('testtest', dbMock)).toEqual([])
-    expect(googleSearch('house', dbMock)).toEqual(['house.com', 'housepics.com'])
-});
+
+
