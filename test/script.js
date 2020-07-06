acc4fcfbd30f924d489a1166b9990cacb20b82b2
dvru <damvaru4@gmail.com>
2020-07-06 14:03:10 -0500

    written google mock test

--- test/script.js
+++ test/script.js
@@ -7,11 +7,13 @@ const googleDatabase = [
     'myfavcars.com'
 ];
 
-const googleSearch = (searchInput) => {
-    const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
         return website.includes(searchInput);
     })
     return matches.length > 3 ? matches.slice(0, 3) : matches
 };
 
-console.log(googleSearch('flow'));
+// console.log(googleSearch('car', googleDatabase));
+
+module.exports = googleSearch;
--- test/script.test.js
+++ test/script.test.js
@@ -1,3 +1,17 @@
+const googleSearch = require('./script');
+
+dbMock = [
+    'house.com',
+    'hats.com',
+    'coffee.com',
+    'housepics.com'
+];
+
 it('this is a test', () => {
-    
-})
+    expect('hello').toBe('hello')
+});
+
+it('it is searching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('house', dbMock)).toEqual(['house.com', 'housepics.com'])
+});
